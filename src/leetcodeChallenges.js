546a0b037760f6c50727b1503cc4e85fcf6c1932
Hayder Jebur <hayderjabur1983@gmail.com>
2021-05-28 11:26:56 -0700

    solve How Many Numbers Are Smaller Than the Current Number

--- src/leetcodeChallenges.js
+++ src/leetcodeChallenges.js
@@ -103,3 +103,25 @@ ParkingSystem.prototype.addCar = function (carType) {
     return true;
   } else return false;
 };
+
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+//That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+// Return the answer in an array.
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+var smallerNumbersThanCurrent = function (nums) {
+  let arr = [];
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] > nums[j]) count++;
+    }
+    arr[i] = count;
+    count = 0;
+  }
+
+  return arr;
+};

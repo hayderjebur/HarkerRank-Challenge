bbf7dc276264764011b12cd3cccdd9bb6d55ce0a
Hayder Jebur <hayderjabur1983@gmail.com>
2021-05-28 19:03:19 -0700

    solve Number of Steps to Reduce a Number to Zero

--- src/leetcodeChallenges.js
+++ src/leetcodeChallenges.js
@@ -147,3 +147,17 @@ var numberOfSteps = function (num) {
   }
   return count;
 };
+
+// 1528. Shuffle String
+// Given a string s and an integer array indices of the same length.
+// The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+// Return the shuffled string.
+var restoreString = function (s, indices) {
+  let arrString = s.split('');
+  let letters = [];
+
+  for (let i = 0; i < arrString.length; i++) {
+    letters[indices[i]] = s.charAt(i);
+  }
+  return letters.join('');
+};

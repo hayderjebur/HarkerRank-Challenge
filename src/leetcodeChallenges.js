f3194418ba10460764c2db755340e3a01c6858de
Hayder Jebur <hayderjabur1983@gmail.com>
2021-06-24 22:20:34 -0700

    slove   Longest Common Prefix

--- src/leetcodeChallenges.js
+++ src/leetcodeChallenges.js
@@ -185,6 +185,24 @@ var isAnagram = function (s, t) {
   }
   return true;
 };
+//14.Longest Common Prefix
+//Write a function to find the longest common prefix string
+//amongst an array of strings.
+//If there is no common prefix, return an empty string "".
+
+var longestCommonPrefix = function (strs) {
+  let prefix = '';
+  for (let i = 0; i < strs[0].length; i++) {
+    let char = strs[0][i];
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[j][i] !== char) {
+        return prefix;
+      }
+    }
+    prefix += char;
+  }
+  return prefix;
+};
 
 // Number Algothims
 //*************************************************************************************
